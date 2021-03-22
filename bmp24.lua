137fd45500ff545caba9ce2cceb0d6750fb47448
ov3rwrite <38213271+ov3rwrite@users.noreply.github.com>
2021-03-22 16:58:06 +0300

    half-symbol pixel drawing added
    Made by Bs0Dd

--- bmp24.lua
+++ bmp24.lua
@@ -2,13 +2,16 @@ local shell = require("shell")
 local filesystem = require("filesystem")
 local internet = require("internet")
 local component = require("component")
+local computer = require("computer")
 local term = require("term")
+local pull = require('event').pull
 local gpu = component.gpu
 
 local args, ops = shell.parse(...)
 
 
 function rgbToHex(rgb)
+  if rgb == nil then return '0X000000' end
   local hexadecimal = '0X'
   for key, value in pairs(rgb) do
     local hex = ''
@@ -44,39 +47,37 @@ img:read(3)
 local h = string.unpack(">B", img:read(1))
 --а тут читаются оставшиеся 31 байт, так как с 1 до 54 байта включительно находится заголовок .bmp
 img:read(31)
-local byte_to_int = nil
 local extrabytes=((res-54)-w*h*3)/h --не спрашивайте зачем, так надо
 local rgb_t = {}
 local px_t = {}
-local r = nil
-local b = nil
-term.clear()
+print("Loading picture...")
+local upt=computer.uptime()
 for i=1, res-54-extrabytes*h do
-  byte_to_int=string.unpack(">B", img:read(1))
-  table.insert(rgb_t, byte_to_int)
+  local byte_to_int=string.unpack(">B", img:read(1))
+  table.insert(rgb_t, 1, byte_to_int)
   if(i%(w*3)==0) then
     img:read(extrabytes)
   end
   if(#rgb_t==3) then
-    r = rgb_t[1]
-    b = rgb_t[3]
-    rgb_t[1]=b
-    rgb_t[3]=r
-    gpu.setForeground(tonumber(rgbToHex(rgb_t)))
     table.insert(px_t, 1, rgb_t)
     rgb_t={}
   end
+  if computer.uptime()>=upt+4 then --при обработке больших картинок цикл должен иногда спать (иначе TLWY)
+	os.sleep()
+	upt=computer.uptime()
+  end
 end
+term.clear()
 local l = 1
-local pc_w, pc_h = gpu.getResolution()
-gpu.fill(1, 1, pc_w, pc_h, " ")
-for k=2, h+1 do
+for k=1, math.ceil(h/2) do
   for j=1, w do
-    gpu.setBackground(tonumber(rgbToHex(px_t[l])))
-    gpu.setForeground(tonumber(rgbToHex(px_t[l])))
-    gpu.set(j, k, "X")
+    gpu.setForeground(tonumber(rgbToHex(px_t[l]))) --передней части символа задаем цвет пикселя с нечетной строки
+	gpu.setBackground(tonumber(rgbToHex(px_t[l+w]))) --задней части символа задаем цвет пикселя с четной строки
+    gpu.set(w-j+1, k, "▀") --рисуем два пикселя в одном символе
     l=l+1
   end
+  l=l+w --перескакиваем на другую нечетную строку
 end
 
 img:close()
+pull('key_down')
